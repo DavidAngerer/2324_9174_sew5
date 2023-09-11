f21ba2eb16cb58f4884957e0d033541c1da9ccc2
David Angelo <david.angelo@htl.rennweg.at>
2023-09-11 16:32:23 +0200

    implemented testing

---
+++ BritishWeight.py
@@ -0,0 +1,40 @@
+class BritishWeight:
+    pass
+
+
+class BritishWeight:
+    """
+    >>> weigth_one = BritishWeight(12,10)
+    >>> weigth_one
+    12 st 10 lb
+    >>> print(weigth_one)
+    12 st 10 lb
+    >>> weigth_two = BritishWeight(0,12)
+    >>> print(weigth_two)
+    12 lb
+    >>> weigth_one + weigth_two
+    13 st 8 lb
+    >>> weigth_two + BritishWeight(0, -1)
+    11 lb
+    """
+    def __init__(self, stone:int, pounds:int):
+        self._pounds = stone * 14 + pounds
+
+    def __str__(self):
+        return f'{str(int(self._pounds/14)) + " st " if self._pounds > 14 else ""}' \
+               f'{str(int(self._pounds%14)) + " lb" if self._pounds % 14 != 0 else "0 lb" if self._pounds == 0 else ""}'
+
+    def __repr__(self):
+        return f'{str(int(self._pounds / 14)) + " st " if self._pounds > 14 else ""}' \
+               f'{str(int(self._pounds % 14)) + " lb" if self._pounds % 14 != 0 else "0 lb" if self._pounds == 0 else ""}'
+
+    @property
+    def pounds(self) -> int:
+        return self._pounds
+
+    @property
+    def stone_and_pounds(self) -> tuple[int,int]:
+        return int(self._pounds/14), self._pounds % 14
+
+    def __add__(self, other:BritishWeight):
+        return BritishWeight(0,self._pounds + other.pounds)
--- BritishWeigth.py
+++
@@ -1,9 +0,0 @@
-class BritishWeight:
-    def __init__(self, stone:int, pounds:int):
-        self._pounds = stone * 14 + pounds
-
-    def __str__(self):
-        return f'{str(int(self._pounds/14)) + " st " if self._pounds > 14 else ""}' \
-               f'{str(int(self._pounds%14)) + " lb " if self._pounds % 14 != 0 else "0 lb" if self._pounds == 0 else ""}'
-
-
