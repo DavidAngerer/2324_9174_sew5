199e54f53a9c17db6d6077bbf45f40c08f31e4c9
David Angelo <david.angelo@htl.rennweg.at>
2023-10-02 15:55:13 +0200

    zwischencommit

--- u01_User_anlegen/create_class.py
+++ u01_User_anlegen/create_class.py
@@ -26,4 +26,20 @@ def get_people_from_name_file(path):
         yield first_name, last_name, group, school_class
 
 
+def create_class_user(path:str,script_file_name:str):
+    if not script_file_name.endswith(".sh"): script_file_name = script_file_name + ".sh"
+    with open(script_file_name, 'w') as file:
+        file.write("#! /bin/sh\n")
+        for class_name, room, kv in get_classes_from_class_file(path):
+            command = f"useradd -d /home/klassen/k{class_name} -G cdrom,plugdev,sambashare k{class_name}"
+
+def get_classes_from_class_file(path:Path):
+    wb = load_workbook(Path(path), read_only=True)
+    ws = wb[wb.sheetnames[0]]
+    for row in ws.iter_rows(min_row=2):
+        class_name = row[0].value
+        room = row[1].value
+        kv = row[2].value
+        yield class_name, room, kv
+
 #create_user_from_name_file("../../res/Namen.xlsx")
