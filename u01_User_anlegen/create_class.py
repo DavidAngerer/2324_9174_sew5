6f59e4d598a87c55241e7d5d6c769c0555bb4034
David Angelo <david.angelo@htl.rennweg.at>
2023-10-02 16:37:41 +0200

    implemented creating

--- u01_User_anlegen/create_class.py
+++ u01_User_anlegen/create_class.py
@@ -1,3 +1,4 @@
+import random
 from pathlib import Path
 from openpyxl import load_workbook
 
@@ -30,16 +31,38 @@ def create_class_user(path:str,script_file_name:str):
     if not script_file_name.endswith(".sh"): script_file_name = script_file_name + ".sh"
     with open(script_file_name, 'w') as file:
         file.write("#! /bin/sh\n")
+        file.write("groupadd klasse\n")
+        user_dict = dict()
         for class_name, room, kv in get_classes_from_class_file(path):
-            command = f"useradd -d /home/klassen/k{class_name} -G cdrom,plugdev,sambashare k{class_name}"
+            if username in user_dict:
+                print("Error: Duplicate User")#TODO
+                break
+            class_name = str(class_name)
+            room = str(room)
+            kv = str(kv)
+            username = f'k{str(class_name).lower()}'
+            command = f"useradd -d /home/klassen/{username} -g klasse -G cdrom,plugdev,sambashare {username}\n"
+            password = f'{class_name}{get_random_pw_char()}{room[0:2]}{get_random_pw_char()}{kv}{get_random_pw_char()}'
+            passwd_command = f'echo {username}:{password} | chpasswd\n'
+            file.write(command)
+            file.write(passwd_command)
 
-def get_classes_from_class_file(path:Path):
+def get_random_pw_char():
+    return "!%&(),._-=^#"[random.randint(0, 11)]
+
+
+def get_classes_from_class_file(path:str):
     wb = load_workbook(Path(path), read_only=True)
     ws = wb[wb.sheetnames[0]]
     for row in ws.iter_rows(min_row=2):
         class_name = row[0].value
         room = row[1].value
         kv = row[2].value
+        if class_name is None:
+            break
         yield class_name, room, kv
 
-#create_user_from_name_file("../../res/Namen.xlsx")
+
+# create_user_from_name_file("../../res/Namen.xlsx")
+create_class_user("../../res/Klassenraeume_2023.xlsx", "script_test")
+
