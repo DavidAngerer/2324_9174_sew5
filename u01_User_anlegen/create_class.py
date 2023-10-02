d21bf7d8dcd083a3858fe2064765a03acf0fc026
David Angelo <david.angelo@htl.rennweg.at>
2023-10-02 16:50:32 +0200

    fixed dumb bug

--- u01_User_anlegen/create_class.py
+++ u01_User_anlegen/create_class.py
@@ -28,25 +28,27 @@ def get_people_from_name_file(path):
 
 
 def create_class_user(path:str):
-    with open("create_users.sh", 'w') as file, open("delete_users.sh") as file_delete, open("userlist"):
+    with open("create_users.sh", 'w') as file, open("delete_users.sh", 'w') as file_delete, \
+            open("userlist.txt", 'w') as user_list_file:
         file.write("#! /bin/sh\n")
         file_delete.write("#! /bin/sh\n")
         file.write("groupadd klasse\n")
         user_dict = dict()
         for class_name, room, kv in get_classes_from_class_file(path):
-            if username in user_dict:
-                print("Error: Duplicate User")#TODO
-                break
             class_name = str(class_name)
             room = str(room)
             kv = str(kv)
             username = f'k{str(class_name).lower()}'
+            if username in user_dict:
+                print("Error: Duplicate User")#TODO
+                break
             command = f"useradd -d /home/klassen/{username} -g klasse -s /bin/sh -G cdrom,plugdev,sambashare {username}\n"
             password = f'{class_name}{get_random_pw_char()}{room[0:2]}{get_random_pw_char()}{kv}{get_random_pw_char()}'
             passwd_command = f'echo {username}:{password} | chpasswd\n'
             file.write(command)
             file.write(passwd_command)
-            file_delete.write(f'userdel -r {username}')
+            file_delete.write(f'userdel -r {username}\n')
+            user_list_file.write(f'username: {username}, password: {password}\n')
 
 def get_random_pw_char():
     return "!%&(),._-=^#"[random.randint(0, 11)]
