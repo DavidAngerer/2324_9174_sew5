19e7717005a2ab2f17099ff3a36e90b1d6186dcf
David Angelo <david.angelo@htl.rennweg.at>
2023-10-16 12:53:44 +0200

    more logging

--- u01_User_anlegen/create_user.py
+++ u01_User_anlegen/create_user.py
@@ -11,51 +11,58 @@ from logging.handlers import RotatingFileHandler
 from logging import StreamHandler
 
 def create_user_from_name_file(path:str):
-    workbook = openpyxl.Workbook()
-    sheet = workbook.active
-    sheet['A1'] = 'Name'
-    sheet['B1'] = 'Password'
-    with open("create_real_users.sh", 'w') as file, open("delete_real_users.sh", 'w') as file_delete, \
-            open("userlist.txt", 'w') as user_list_file:
-        file.write("#! /bin/sh\n")
-        file_delete.write("#! /bin/sh\n")
-        file.write("groupadd schueler\n")
-        user_dict = dict()
-        index = 2
-        for first_name, last_name, group, school_class in get_people_from_name_file(path):
-            username = replace_umlaute_and_remove_accents(last_name).replace(" ", "_")
-            if username in user_dict:
-                user_dict[username] += 1
-                username = username+ f"_{user_dict[username]}"
-            else:
-                user_dict[username] = 0
-            command = f"useradd -d \"/home/klassen/{username}\" -g schueler -c \"{username} - {school_class}\" " \
-                      f"-s /bin/bash -G cdrom,plugdev,sambashare {username}\n"
-            password = get_random_password()
-            file.write(command)
-            logger.debug(f"User created: {username}")
-            passwd_command = f'echo {username}:{password} | chpasswd\n'
-            file.write(passwd_command)
-            logger.debug(f"User password created for {username}")
-            file_delete.write(f'userdel -r {username}\n')
-            logger.debug(f"User deletion written for {username}")
-            user_list_file.write(f'username: {username}, password: {password}\n')
-            logger.debug(f"User \"{username}\" added to list")
-            sheet[f'A{index}'] = username
-            sheet[f'B{index}'] = password
-            index+=1
-    workbook.save('users.xlsx')
-
+    try:
+        workbook = openpyxl.Workbook()
+        sheet = workbook.active
+        sheet['A1'] = 'Name'
+        sheet['B1'] = 'Password'
+        with open("create_real_users.sh", 'w') as file, open("delete_real_users.sh", 'w') as file_delete, \
+                open("userlist.txt", 'w') as user_list_file:
+            file.write("#! /bin/sh\n")
+            file_delete.write("#! /bin/sh\n")
+            file.write("groupadd schueler\n")
+            user_dict = dict()
+            index = 2
+            for first_name, last_name, group, school_class in get_people_from_name_file(path):
+                username = replace_umlaute_and_remove_accents(last_name).replace(" ", "_")
+                if username in user_dict:
+                    user_dict[username] += 1
+                    username = username+ f"_{user_dict[username]}"
+                else:
+                    user_dict[username] = 0
+                command = f"useradd -d \"/home/klassen/{username}\" -g schueler -c \"{username} - {school_class}\" " \
+                          f"-s /bin/bash -G cdrom,plugdev,sambashare {username}\n"
+                password = get_random_password()
+                file.write(command)
+                logger.debug(f"User created: {username}")
+                passwd_command = f'echo {username}:{password} | chpasswd\n'
+                file.write(passwd_command)
+                logger.debug(f"User password created for {username}")
+                file_delete.write(f'userdel -r {username}\n')
+                logger.debug(f"User deletion written for {username}")
+                user_list_file.write(f'username: {username}, password: {password}\n')
+                logger.debug(f"User \"{username}\" added to list")
+                sheet[f'A{index}'] = username
+                sheet[f'B{index}'] = password
+                logger.debug(f"Wrote user {username} in excel")
+                index+=1
+        workbook.save('users.xlsx')
+    except Exception:
+        logger.critical("Couldn't write in one of the Files")
 
 def get_people_from_name_file(path):
-    wb = load_workbook(Path(path), read_only=True)
-    ws = wb[wb.sheetnames[0]]
-    for row in ws.iter_rows(min_row=2):
-        first_name = row[0].value
-        last_name = row[1].value
-        group = row[2].value
-        school_class = row[3].value
-        yield first_name, last_name, group, school_class
+    try:
+        wb = load_workbook(Path(path), read_only=True)
+        ws = wb[wb.sheetnames[0]]
+        logger.debug("Read from excel")
+        for row in ws.iter_rows(min_row=2):
+            first_name = row[0].value
+            last_name = row[1].value
+            group = row[2].value
+            school_class = row[3].value
+            yield first_name, last_name, group, school_class
+    except Exception:
+        logger.critical("Couln't read excel file")
 
 
 def get_random_password() -> str:
